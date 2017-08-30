f30711c45bf02b5b7a13a27a1e53a00ca0a16b78
LiYongFeng0101 <1297499770@qq.com>
2017-08-30 12:14:49 +0800

    Add files via upload

---
+++ js/2.js
@@ -0,0 +1,15 @@
+// window.onload = function(){
+// 	var next1;
+// 	var timer1;
+// 	var contain1=document.getElementById('run_img');
+// 	var list1=document.getElementById('img0');
+// 	next1=function(){
+// 		list.style.left=parseInt(list.style.left)-1226+"px";
+// 	}
+// 	function play1(){
+// 		timer=setInterval(function(){
+// 			next1();
+// 		},3000)
+// 		play1();
+// 	}
+// }
